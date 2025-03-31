754986f792cf0ca1396bccecfa21b5d4547a1b9d
Juan Solis <juansolis43@gmail.com>
2025-03-31 08:00:41 -0600

    Fix Proxy for metricaltree

--- metrical-tree-client/src/setupProxy.js
+++ metrical-tree-client/src/setupProxy.js
@@ -1,22 +1,24 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  // Create a WebSocket proxy for hot module replacement
-  app.use(
-    '/ws',
-    createProxyMiddleware({
-      target: 'http://localhost:3000',
-      ws: true,
-      changeOrigin: true,
-    })
-  );
+  if (process.env.NODE_ENV === 'development') {
+    // Create a WebSocket proxy for hot module replacement
+    app.use(
+      '/ws',
+      createProxyMiddleware({
+        target: 'http://localhost:3000',
+        ws: true,
+        changeOrigin: true,
+      })
+    );
 
-  // Proxy API requests to the backend
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: process.env.REACT_APP_API_URL || 'http://api.local',
-      changeOrigin: true,
-    })
-  );
+    // Proxy API requests to the backend
+    app.use(
+      '/api',
+      createProxyMiddleware({
+        target: process.env.REACT_APP_API_URL || 'http://api.local',
+        changeOrigin: true,
+      })
+    );
+  }
 };
